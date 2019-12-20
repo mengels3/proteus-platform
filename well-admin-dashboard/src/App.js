65bcb2a560763821034f0546f792e6a8773a52f7
pschuetz <paulschuetz.mail@gmail.com>
2019-12-20 16:38:47 +0100

    work on admin dashboard

--- well-admin-dashboard/src/App.js
+++ well-admin-dashboard/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import MeasurementListItem from './components/MeasurementListItem'
 
-const data = [{name: "Chile-01", value: 12}, {name: "Chile-02", value: 13}]
+const data = [{name: "Chile-01", id: "e34810f0-8471-495b-aa3e-a338dc8b8de2", value: 12}, {name: "Chile-02", id: "7c56dcca-1ec2-4b9c-9455-77830fc313e2", value: 13}]
 
 function App() {
   return (
--- well-admin-dashboard/src/components/MeasurementListItem.js
+++ well-admin-dashboard/src/components/MeasurementListItem.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import './MeasurementListItem.css'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faTrash, faPen} from '@fortawesome/free-solid-svg-icons'
+import {faTrash, faPen, faThermometerHalf, faTint, faWater} from '@fortawesome/free-solid-svg-icons'
 
 class MeasurementListItem extends React.Component {
+
     render() {
-      return <div class="rounded-borders">
-                <b class="text">{this.props.data.name}</b>
-                <div class="icon-div">
-                    <div class="list-item-icon"><FontAwesomeIcon icon={faPen}/></div>
-                    <div class="list-item-icon"><FontAwesomeIcon icon={faTrash}/></div>
+      return <div class="measurement-item-background">
+                    <div class="measurement-item-overview">
+                        <div class="measurement-item-overview-left">
+                            <div class="column">
+                                <b class="text">{this.props.data.name}</b>
+                                <p class="detail-text"> {this.props.data.id}</p>
+                            </div>
+                            <div class="column margin-left">
+                                <b class="measurements-text">Measurements</b>
+                                <div class="measurements-listing-row">
+                                    <div class="measurements-icon"><FontAwesomeIcon icon={faThermometerHalf}/></div>
+                                    <p class="text">Temperature</p>
+                                    <div class="measurements-icon"><FontAwesomeIcon icon={faTint}/></div>
+                                    <p class="text">pH</p>
+                                    <div class="measurements-icon"><FontAwesomeIcon icon={faWater}/></div>
+                                    <p class="text">Water level</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="icon-div">
+                            <div class="list-item-icon" onClick={() => this.setState({visible:true})}><FontAwesomeIcon icon={faPen}/></div>
+                            <div class="list-item-icon"><FontAwesomeIcon icon={faTrash}/></div>
+                        </div>
                 </div>
-            </div>;
+            </div>
+            ;
     }
 }
 
