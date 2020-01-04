1a9feda2f943e97b91200f68c754ad961b6fb1ad
pschuetz <paulschuetz.mail@gmail.com>
2020-01-04 17:49:48 +0100

    refactor

--- well-admin-dashboard/src/App.js
+++ well-admin-dashboard/src/App.js
@@ -5,50 +5,11 @@ import EditMeasurementModalContainer from "./container-components/EditMeasuremen
 
 class App extends React.Component {
 
-  showEditModal(data) {
-    this.setState({
-      selectedData: data,
-      showEditModal: true
-    });
-  }
-
-  onEditModalSave(updatedMeasurementItem) {
-    const replaceIndex = this.state.data.findIndex(item => item.id === updatedMeasurementItem.id);
-    console.log(replaceIndex);
-
-    var dataCopy = this.state.data.slice();
-    dataCopy[replaceIndex] = updatedMeasurementItem;
-
-    console.log("save");
-    console.log(dataCopy);
-
-    this.setState({
-      showEditModal: false,
-      data: dataCopy
-    });
-  }
-
-  hideEditModal() {
-    this.setState({ showEditModal: false });
-  }
-
-  deleteMeasurementPoint(id) {
-    console.log("delete: " + id);
-    const deleteIndex = this.state.data.findIndex(item => item.id === id);
-    const dataCopy = this.state.data.slice();
-
-    dataCopy.splice(deleteIndex, 1);
-
-    this.setState({
-      data: dataCopy
-    });
-  }
-
   render() {
     return (
       <>
-        <MeasurementPointListContainer/>
-        <EditMeasurementModalContainer/>
+        <MeasurementPointListContainer />
+        <EditMeasurementModalContainer />
       </>
     );
   }
