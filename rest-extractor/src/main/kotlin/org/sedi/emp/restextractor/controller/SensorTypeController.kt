2857d5eaa0c45f54dcbcbea7dcaa52d68c55c9ab
patrickp89 <patrickpreuss@netherspace.de>
2020-01-08 10:30:52 +0100

    wip: all sensor types endpoint

---
+++ rest-extractor/src/main/kotlin/org/sedi/emp/restextractor/controller/SensorTypeController.kt
@@ -0,0 +1,23 @@
+package org.sedi.emp.restextractor.controller
+
+import org.sedi.emp.restextractor.model.masterdata.SensorType
+import org.sedi.emp.restextractor.persistence.SensorTypeRepository
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class SensorTypeController(private val sensorTypeRepository: SensorTypeRepository) {
+
+    private val logger = LoggerFactory.getLogger(SensorTypeController::class.java)
+
+    @GetMapping("/sensor-type")
+    fun allSensorTypes(): List<SensorType> {
+        val sensorTypes = sensorTypeRepository
+                .findAll()
+                .toList()
+
+        logger.debug("Returning ${sensorTypes.size} sensor types")
+        return sensorTypes
+    }
+}
