68124675529667ab43bb11b1740d3329e5406bd6
patrickp89 <patrickpreuss@netherspace.de>
2020-01-08 10:12:33 +0100

    wip: IDs are now UUIDs

--- rest-extractor/src/main/kotlin/org/sedi/emp/restextractor/controller/WellController.kt
+++ rest-extractor/src/main/kotlin/org/sedi/emp/restextractor/controller/WellController.kt
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 class WellController(private val wellRepository: WellRepository) {
@@ -26,7 +27,7 @@ class WellController(private val wellRepository: WellRepository) {
     }
 
     @GetMapping("/well/{id}/measurements")
-    fun getMeasurementsForWell(@PathVariable id: Long): ResponseEntity<List<Measurement>> {
+    fun getMeasurementsForWell(@PathVariable id: UUID): ResponseEntity<List<Measurement>> {
         logger.debug("Looking up well with ID $id")
         val maybeWell = wellRepository.findById(id)
         return if (maybeWell.isPresent) {
