d715d3a2aa46e0a4c163c7a317558a5c4f07e87c
pschuetz <paulschuetz.mail@gmail.com>
2020-01-15 10:55:06 +0100

    added icon and some coloring

--- well-admin-dashboard/src/App.js
+++ well-admin-dashboard/src/App.js
@@ -1,15 +1,30 @@
 import React from "react";
 import "./App.css";
 import MeasurementPointListContainer from './container-components/MeasurementPointListContainer'
-import EditMeasurementModalContainer from "./container-components/EditMeasurementModalContainer";
+import EditMeasurementModalContainer from "./container-components/EditMeasurementModalContainer"
 import AddMeasurementPointButton from './presentational-components/AddMeasurementPointButton'
 import AddMeasurementPointModal from './presentational-components/AddMeasurementPointModal'
+import { Navbar } from 'react-bootstrap'
+import drop from './assets/drop.png'
+
 
 class App extends React.Component {
 
   render() {
     return (
       <>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src={drop}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            Paricia Well Management
+    </Navbar.Brand>
+        </Navbar>
         <MeasurementPointListContainer />
         <AddMeasurementPointButton />
         <EditMeasurementModalContainer />
